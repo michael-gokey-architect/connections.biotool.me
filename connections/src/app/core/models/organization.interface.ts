7dfb6f9fd0883bfc68ab67a2c53b863079e91a68
michael-gokey-architect <michael.gokey.architect@gmail.com>
2024-06-16 23:30:12 -0400

    changing interfaces

---
+++ connections/src/app/core/models/organization.interface.ts
@@ -0,0 +1,20 @@
+export interface Organization {
+    organization_id: number,
+    organization_name: string,
+    description: string | null,
+    address_line1: string | null,
+    address_line2: string | null,
+    city: string | null,
+    state_province: string | null,
+    postal_code: string | null,
+    country: string | null,
+    email: string | null,
+    phone_number: string | null,
+    website: string | null,
+    point_of_contact_name: string | null,
+    point_of_contact_email: string | null,
+    point_of_contact_phone: string | null,
+    deleted_date: string | null
+
+}
+
