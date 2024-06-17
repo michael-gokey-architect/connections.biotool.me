03ce363d54a101a2c91a5c4a78935cbb78509a84
michael-gokey-architect <michael.gokey.architect@gmail.com>
2024-06-16 22:14:12 -0400

    setting theme in place

--- connections/src/app/features/features.module.ts
+++ connections/src/app/features/features.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.component';
 import { FeatureLandingComponent } from './feature-landing/feature-landing.component';
 import { UserAvatarUploaderComponent } from './user-avatar-uploader/user-avatar-uploader.component';
+import { ManageAliasComponent } from './manage-alias/manage-alias.component';
 
 
 
@@ -10,7 +11,8 @@ import { UserAvatarUploaderComponent } from './user-avatar-uploader/user-avatar-
   declarations: [
     ThemeSwitcherComponent,
     FeatureLandingComponent,
-    UserAvatarUploaderComponent
+    UserAvatarUploaderComponent,
+    ManageAliasComponent
   ],
   imports: [
     CommonModule
--- connections/src/app/features/theme-switcher/theme-switcher.component.ts
+++ connections/src/app/features/theme-switcher/theme-switcher.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-theme-switcher',
   templateUrl: './theme-switcher.component.html',
   styleUrls: ['./theme-switcher.component.css']
 })
-export class ThemeSwitcherComponent {
+export class ThemeSwitcherComponent  implements OnInit  {
+  headerFooter = false;
+  theme: string = 'theme-grays';
+
+	// onThemeUpdate(updated: boolean) {
+	// 	if (updated) this.theme = localStorage.getItem('theme')!;
+	// }
+	
+	ngOnInit() {
+		// localStorage.getItem('theme') === null
+    //   ? localStorage.setItem('theme', this.theme)
+    //   : (this.theme = localStorage.getItem('theme')!);;
+	}
+
+
 
 }
