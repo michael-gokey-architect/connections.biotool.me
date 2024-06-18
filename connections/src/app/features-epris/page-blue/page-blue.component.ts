6807e28bd51abbd260ad393dcdb1f721cdfec1f6
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-17 20:21:18 -0400

    Move Font Import to Global CSS

--- connections/src/app/features-epris/page-blue/page-blue.component.ts
+++ connections/src/app/features-epris/page-blue/page-blue.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-page-blue',
   templateUrl: './page-blue.component.html',
-  styleUrls: ['./page-blue.component.css']
+	styleUrls: ['./page-blue.component.css'],
+	encapsulation: ViewEncapsulation.None
 })
 export class PageBlueComponent {
 
