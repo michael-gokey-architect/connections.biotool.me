c62b636ad68312905093fc5e1fa82cd1c149a5fa
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-18 08:09:26 -0400

    Update Links in Links Panel to Buttons

--- connections/src/app/features-epris/page-blue/page-blue.component.ts
+++ connections/src/app/features-epris/page-blue/page-blue.component.ts
@@ -8,4 +8,14 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class PageBlueComponent {
 	userFullName = "John Bianchi"
+
+	onLinkedInClick() {
+		window.open('https://www.linkedin.com/in/bianchijohn/', '_blank');
+	}
+	onPatchWRKClick() {
+		window.open('https://www.circlepass.io/patchwrq', '_blank');
+	}
+	onQuestAppClick() {
+		window.open('https://www.circlepass.io/quest', '_blank');
+	}
 }
