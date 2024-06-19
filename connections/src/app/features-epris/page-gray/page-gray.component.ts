21ecb53544927cf6107743a189a874088a8912ce
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-18 21:06:41 -0400

    Update Grays Theme CSS

--- connections/src/app/features-epris/page-gray/page-gray.component.ts
+++ connections/src/app/features-epris/page-gray/page-gray.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-page-gray',
   templateUrl: './page-gray.component.html',
   styleUrls: ['./page-gray.component.css'],
+	encapsulation: ViewEncapsulation.None
 })
 export class PageGrayComponent {
   userFullName = 'John Bianchi';
