dfb25aafef8b086efa4e63a5329677b106cb316a
SidAngular1996 <sid17tiwari@gmail.com>
2024-06-24 21:56:09 +0530

    update org details with some skeleton changes in create delete get

--- connections/src/app/organization/update-organization/update-organization.component.ts
+++ connections/src/app/organization/update-organization/update-organization.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { OrganizationService } from 'src/app/core/services/organization.service';
 
 @Component({
   selector: 'app-update-organization',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-organization.component.css']
 })
 export class UpdateOrganizationComponent {
+  organizationUpdatedForm: FormGroup;
+
+  constructor(private orgService: OrganizationService, private fb: FormBuilder) {
+    //basic validators need to put check in UI
+    this.organizationUpdatedForm = this.fb.group({
+      organization_id: ['', Validators.required],
+      organization_name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      point_of_contact_name: ['', Validators.required],
+      address_line1: ['', Validators.required]
+    });
+  }
+
+  onSubmit(event: Event): void {
+    //keep validation check for form
+      event.preventDefault();
+
+      console.log("Org Updated Form Value", this.organizationUpdatedForm.value);
+      //use subscribe with next and error object as per new standard
+      this.orgService.updateOrganization(this.organizationUpdatedForm.value).subscribe({
+        next: response => {
+          console.log('Updated Form submitted successfully:', response);
+          this.organizationUpdatedForm.reset();
+        },
+        error: error => {
+          console.error('Error submitting form:', error);
+        },
+        complete: () => {
+          console.log('Updated Org completed');
+        }
+      });
+  }
 
 }
