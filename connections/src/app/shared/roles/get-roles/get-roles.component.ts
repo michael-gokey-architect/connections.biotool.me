5f8fada286d85e7ba258e7bc63c07b47dabd78bb
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-26 20:10:47 -0400

    Build Get Role

--- connections/src/app/shared/roles/get-roles/get-roles.component.ts
+++ connections/src/app/shared/roles/get-roles/get-roles.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Roles } from 'src/app/core/models/roles.interface';
+import { RolesService } from 'src/app/core/services/roles.service';
 
 @Component({
   selector: 'app-get-roles',
   templateUrl: './get-roles.component.html',
-  styleUrls: ['./get-roles.component.css']
+  styleUrls: ['./get-roles.component.css'],
 })
 export class GetRolesComponent {
+  roles: Roles[] = [];
 
+  constructor(private orgService: RolesService) {}
+
+  ngOnInit(): void {
+    //get all roles
+    this.orgService.getUserRoles().subscribe({
+      next: (data) => {
+        this.roles = data;
+        console.log('Roles:', this.roles);
+      },
+      error: (error) => {
+        console.error('Error fetching roles:', error);
+      },
+      complete: () => {
+        console.log('Request to fetch Org completed');
+      },
+    });
+  }
 }
