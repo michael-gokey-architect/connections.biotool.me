84738f66946a537e75e3f706b3e36139a191795b
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-26 20:06:25 -0400

    Build Create Role

--- connections/src/app/core/services/roles.service.ts
+++ connections/src/app/core/services/roles.service.ts
@@ -1,9 +1,68 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Roles } from '../models/roles.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RolesService {
+  private apiURL = 'http://api.mozli.com/Biodata';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  getUserRoles(): Observable<any> {
+    return this.httpClient
+      .get(this.apiURL + '/GetUserRoles/')
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  createUserRole(role: Roles): Observable<any> {
+    return this.httpClient
+      .post(
+        this.apiURL + '/AddUserRole/',
+        JSON.stringify(role),
+        this.httpOptions
+      )
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  deleteUserRoles(role_id: number): Observable<any> {
+    return this.httpClient
+      .delete(this.apiURL + '/DeleteUserRole/' + role_id)
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  //we need to pass org id as a query parameter to update
+  //right now org id is given as req body
+  updateUserRole(role: Roles): Observable<any> {
+    return this.httpClient
+      .put(
+        this.apiURL + '/UpdateUserRole/',
+        JSON.stringify(role),
+        this.httpOptions
+      )
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => new Error(error.message || 'Server Error'));
+  }
 }
