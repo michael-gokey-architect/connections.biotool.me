1e18f14c9f1878ebf7215b41d77b359ed8b4cdf3
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-26 20:48:27 -0400

    Build Create Tag

--- connections/src/app/core/services/tags.service.ts
+++ connections/src/app/core/services/tags.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Tags } from '../models/tags.interface';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TagsService {
+  private apiURL = 'http://api.mozli.com/Biodata';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  getTags(): Observable<any> {
+    return this.httpClient
+      .get(this.apiURL + '/GetTags/')
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  createTag(tag: Tags): Observable<any> {
+    return this.httpClient
+      .post(
+        this.apiURL + '/AddTag/',
+        JSON.stringify(tag),
+        this.httpOptions
+      )
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  //we need to pass org id as a query parameter to update
+  //right now org id is given as req body
+  updateTag(tag: Tags): Observable<any> {
+    return this.httpClient
+      .put(
+        this.apiURL + '/UpdateTag/',
+        JSON.stringify(tag),
+        this.httpOptions
+      )
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => new Error(error.message || 'Server Error'));
+  }
 }
