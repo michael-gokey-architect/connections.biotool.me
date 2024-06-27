9de13cd2eb7a5562a8cf170351bf1abd7c9d3d9a
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-26 20:54:41 -0400

    Build Delete Tag

--- connections/src/app/shared/tags/delete-tags/delete-tags.component.ts
+++ connections/src/app/shared/tags/delete-tags/delete-tags.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TagsService } from 'src/app/core/services/tags.service';
 
 @Component({
   selector: 'app-delete-tags',
   templateUrl: './delete-tags.component.html',
-  styleUrls: ['./delete-tags.component.css']
+  styleUrls: ['./delete-tags.component.css'],
 })
 export class DeleteTagsComponent {
+  deleteForm!: FormGroup;
 
+  constructor(
+    private tagService: TagsService,
+    private fb: FormBuilder
+  ) {
+    this.deleteForm = this.fb.group({
+      tagId: ['', [Validators.required]],
+    });
+  }
+
+  deleteTag(): void {
+    if (this.deleteForm.valid) {
+			const id = this.deleteForm.value.orgId;
+			console.log("Attempted Tag Delete")
+      // this.tagService.deleteTags(id).subscribe({
+      //   next: (response) => {
+      //     console.log('Tag deleted successfully:', response);
+      //     this.deleteForm.reset();
+      //   },
+      //   error: (error) => {
+      //     console.error('Error deleting tag:', error);
+      //   },
+      // });
+    }
+  }
 }
