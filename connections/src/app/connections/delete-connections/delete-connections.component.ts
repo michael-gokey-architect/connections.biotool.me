ed95cfaa79c7cb26fb18581c48c3024c61dac0f4
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-27 20:59:17 -0400

    Generate Delete Component

---
+++ connections/src/app/connections/delete-connections/delete-connections.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-connections',
+  templateUrl: './delete-connections.component.html',
+  styleUrls: ['./delete-connections.component.css']
+})
+export class DeleteConnectionsComponent {
+
+}
