4bdea6434c69125068007fe73f3fd6ba46e92532
michael-gokey-architect <michael.gokey.architect@gmail.com>
2024-06-28 16:37:55 -0400

    tailwind

--- connections/tailwind.config.js
+++ connections/tailwind.config.js
@@ -13,28 +13,6 @@ module.exports = {
 
   theme: {
     extend: {
-      colors: {
-        theme1: {
-          primary: "#1DA1F2",
-          secondary: "#14171A",
-          tertiary: "#657786",
-          quaternary: "#AAB8C2",
-          quinary: "#E1E8ED",
-          senary: "#F5F8FA",
-          "label-titles": "#000000",
-          "font-color": "#ffffff",
-        },
-        theme2: {
-          primary: "#FF6347",
-          secondary: "#8B0000",
-          tertiary: "#B22222",
-          quaternary: "#FF4500",
-          quinary: "#FFA07A",
-          senary: "#FF7F50",
-          "label-titles": "#000000",
-          "font-color": "#ffffff",
-        },
-      },
       fontFamily: {
         theme1: {
           sans: ["Graphik", "sans-serif"],
