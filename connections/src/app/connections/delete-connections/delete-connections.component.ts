c75c828f636f9943cafbe865cfc97afd4bbb0bc3
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-27 21:10:18 -0400

    Build Delete Connection

--- connections/src/app/connections/delete-connections/delete-connections.component.ts
+++ connections/src/app/connections/delete-connections/delete-connections.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConnectionService } from 'src/app/core/services/connection.service';
+
 
 @Component({
   selector: 'app-delete-connections',
   templateUrl: './delete-connections.component.html',
-  styleUrls: ['./delete-connections.component.css']
+  styleUrls: ['./delete-connections.component.css'],
 })
 export class DeleteConnectionsComponent {
+  deleteForm!: FormGroup;
 
+  constructor(
+    private connService: ConnectionService,
+    private fb: FormBuilder
+  ) {
+    this.deleteForm = this.fb.group({
+      connId: ['', [Validators.required]],
+    });
+  }
+
+  deleteConnection(): void {
+    if (this.deleteForm.valid) {
+      const id = this.deleteForm.value.connId;
+      this.connService.deleteConnections(id).subscribe({
+        next: (response) => {
+          console.log('Connection deleted successfully:', response);
+          this.deleteForm.reset();
+        },
+        error: (error) => {
+          console.error('Error deleting connection:', error);
+        },
+      });
+    }
+  }
 }
