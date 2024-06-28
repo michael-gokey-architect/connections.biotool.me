128018af64f0c0de43c50e759d5534c36d41c23e
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-06-27 20:07:56 -0400

    Generate Event CRUD Components

---
+++ connections/src/app/events/get-events/get-events.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-events',
+  templateUrl: './get-events.component.html',
+  styleUrls: ['./get-events.component.css']
+})
+export class GetEventsComponent {
+
+}
