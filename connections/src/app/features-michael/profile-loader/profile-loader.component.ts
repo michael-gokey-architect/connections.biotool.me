3650c1a3a80e3a1758028ffc1c9e13853cd7c32f
eyln01 <eyln123@gmail.com>
2024-07-08 20:33:31 -0400

    Adding in stub for profile loader

--- connections/src/app/features-michael/profile-loader/profile-loader.component.ts
+++ connections/src/app/features-michael/profile-loader/profile-loader.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PiiService } from 'src/app/core/services/pii.service';
 
 @Component({
   selector: 'app-profile-loader',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-loader.component.css']
 })
 export class ProfileLoaderComponent {
+  inputForm: FormGroup;
+  bio: string;
 
+  constructor(private fb: FormBuilder, private piiService: PiiService,){
+    this.inputForm = this.fb.group({
+      userId: ['', Validators.required]
+    });
+    this.bio = '';
+  }
+
+  onSubmit(event: Event): void {
+    //keep validation check for form
+    // if (this.piiform.valid) {
+    event.preventDefault();
+
+    console.log("User Id Value", this.inputForm.value);
+    const id = this.inputForm.value.userId;
+    this.piiService.getUserPii(id).subscribe({
+      next: response => {
+        this.bio = response.biography;
+        console.log('Bio get successfully:', this.bio);
+        this.inputForm.reset();
+      },
+      error: error => {
+        console.error('Error:', error);
+      }
+    })
+
+    //create pii call 
+    //use subscribe with next and error object as per new standard
+    
+  }
 }
