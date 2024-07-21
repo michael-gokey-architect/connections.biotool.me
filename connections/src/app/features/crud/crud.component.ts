bb37a08f2b6adad4aec1814d5742b9c7e91d781c
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-20 21:40:51 -0400

    Update Theme onChange

--- connections/src/app/features/crud/crud.component.ts
+++ connections/src/app/features/crud/crud.component.ts
@@ -14,29 +14,31 @@ export class CrudComponent {
   userBio =
     'Lorem ipsum dolor sit amet, consectetur Caramels are umm. Vivamus nec tincidunt urna, sed feugiat metus. mollis eget nunc. Integer eu metus ut lectus luctus tempor. Donec sed nisl pretium, porttitor risus eu, lobortis leo. Nullam laoreet vulputate magna sit amet laoreet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus iaculis imperdiet ex, sit amet molestie justo mollis eu. Aenean volutpat diam dui, vitae interdum mi placerat eget. Curabitur at nibh a sem faucibus accumsan. Nullam elementum, risus nec finibus iaculis, erat orci vehicula est, quis fringilla justo tellus vitae metus.';
   envName!: string;
-  theme: string = localStorage.getItem('theme') || '';
+	theme: string = localStorage.getItem('theme') || '';
+	selected: string = '';
 
   constructor(private router: Router, private connection: ConnectionService) {
     this.envName = this.connection.getEnvironment();
   }
 
   ngOnInit() {
-    if (this.theme) document.body.dataset['theme'] = this.theme;
+		if (this.theme) {
+			document.body.dataset['theme'] = this.theme;
+			this.selected = this.theme
+		}
 
-    let switcher = document.getElementById(
-      'themeoptions'
-    )! as HTMLSelectElement;
-
-    switcher?.addEventListener('change', (e: Event) => {
-      let selectedThemeIndex: number = switcher.options.selectedIndex;
-      let switcherTheme =
-        switcher.options[selectedThemeIndex].getAttribute('value');
-      const selectedTheme = switcherTheme || 'grey';
-      localStorage.setItem('theme', selectedTheme);
-      this.theme = selectedTheme;
-      document.body.dataset['theme'] = switcherTheme!;
-    });
-  }
+		if (!this.theme) {
+			localStorage.setItem('theme', 'grey');
+			this.selected = 'grey';
+			document.body.dataset['theme'] = 'grey';
+		}
+	}
+	
+	updateTheme(value: string) {
+    localStorage.setItem('theme', value);
+    this.theme = value;
+    document.body.dataset['theme'] = value;
+	}
 
   onCreateOrganization() {
     //using router to navigate on same page
