e43b46f4b29d999af500c35e33f35d8c7886b8a0
michael-gokey-architect <michael.gokey.architect@gmail.com>
2024-08-07 12:40:32 -0400

    clean up

--- connections/src/environments/environment.prod.ts
+++ connections/src/environments/environment.prod.ts
@@ -2,17 +2,25 @@ export const environment = {
     production: true,
     environmentName: "prod",
 
-apiUrl_user:            'http://api.mozli.com/UserProfile',
-apiUrl_event:           'http://api.mozli.com/Event',
-apiUrl_organization:    'http://api.mozli.com/Organization',
-apiUrl_connection:      'http://api.mozli.com/Social',
-apiUrl_profilePicture:  'http://localhost:2000/profile-picture',
-apiUrl_tags:            'http://api.mozli.com/Tag',
-apiUrl_userPii:         'http://api.mozli.com/Pii',
-apiUrl_socialMediLink:  'http://api.mozli.com/Link',
-apiUrl_roles:           'http://api.mozli.com/Role',
-apiUrl_themes:          'http://api.mozli.com/Theme',
-apiUrl_badges:          'http://api.mozli.com/Badge'
-  
-}
+    apiUrl_badge:           'http://api.mozli.com/Badge',
+    apiUrl_biodata:         'http://api.mozli.com/Biodata',
+    apiUrl_connection:      'http://api.mozli.com/Connection',
+    apiUrl_event:           'http://api.mozli.com/Event',
+    apiUrl_journey:         'http://api.mozli.com/Journey',
+    apiUrl_journeyReward:   'http://api.mozli.com/JourneyReward',
+    apiUrl_socialMediLink:  'http://api.mozli.com/Link',
+    apiUrl_organization:    'http://api.mozli.com/Organization',
+    apiUrl_orgUser:         'http://api.mozli.com/OrgUser',
+    apiUrl_userPii:         'http://api.mozli.com/Pii',
+    apiUrl_qrCode:          'http://api.mozli.com/QRCode',
+    apiUrl_userRole:        'http://api.mozli.com/Role',
+    apiUrl_tags:            'http://api.mozli.com/Tag',
+    apiUrl_themes:          'http://api.mozli.com/Theme',
+    apiUrl_user:            'http://api.mozli.com/UserProfile',
+
+    apiUrl_vendor:          'http://localhost:2000/vendor',    
+    apiUrl_profilePicture:  'http://localhost:2000/profile-picture',
+    apiUrl_userSelectedTag: 'http://localhost:2000/userSelectedTags',
+    
+  };
   
--- connections/src/environments/environment.testing.ts
+++ connections/src/environments/environment.testing.ts
@@ -2,11 +2,25 @@ export const environment = {
     production: false,
     environmentName: "testing",
     
-    apiUrl_user:            'http://localhost:2000/users',
-    apiUrl_event:           'http://localhost:2000/events',
-    apiUrl_organization:    'http://localhost:2000/organization'
+    apiUrl_badge:           'http://api.mozli.com/Badge',
+    apiUrl_biodata:         'http://api.mozli.com/Biodata',
+    apiUrl_connection:      'http://api.mozli.com/Connection',
+    apiUrl_event:           'http://api.mozli.com/Event',
+    apiUrl_journey:         'http://api.mozli.com/Journey',
+    apiUrl_journeyReward:   'http://api.mozli.com/JourneyReward',
+    apiUrl_socialMediLink:  'http://api.mozli.com/Link',
+    apiUrl_organization:    'http://api.mozli.com/Organization',
+    apiUrl_orgUser:         'http://api.mozli.com/OrgUser',
+    apiUrl_userPii:         'http://api.mozli.com/Pii',
+    apiUrl_qrCode:          'http://api.mozli.com/QRCode',
+    apiUrl_userRole:        'http://api.mozli.com/Role',
+    apiUrl_tags:            'http://api.mozli.com/Tag',
+    apiUrl_themes:          'http://api.mozli.com/Theme',
+    apiUrl_user:            'http://api.mozli.com/UserProfile',
+
+    apiUrl_vendor:          'http://localhost:2000/vendor',    
+    apiUrl_profilePicture:  'http://localhost:2000/profile-picture',
+    apiUrl_userSelectedTag: 'http://localhost:2000/userSelectedTags',
     
-    }
-    
-    // Dont forget to copy over the parts in angular
+  };
     
